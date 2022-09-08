438283375d73a36ad94eeac390f36f6b336ca58d
vivelakorea <gyujang95@snu.ac.kr>
2022-09-08 22:23:47 +0900

    delete matlab files

--- ch2/line_segments.py
+++ ch2/line_segments.py
@@ -1,30 +1,57 @@
 import plotly.graph_objects as go
 import random
 
-# fig = go.Figure()
 
-# fig.add_trace(go.Scatter(x=[0,1,3], y=[0,1,1], mode='lines'))
+### generate set S
 
-# fig.show()
+diversity = 1000
+num_of_points = 40
 
 points = []
-for _ in range(100):
+for _ in range(diversity):
     x = random.random()
     y = random.random()
     points.append([x, y])
 
-S = set()
-for _ in range(20):
-    i = random.randrange(100)
-    j = random.randrange(100)
-    S.add((i, j))
+S = []
+cnt = 0
+while cnt < num_of_points:
+    i = random.randrange(diversity)
+    j = random.randrange(diversity)
+    if i == j: continue
 
-print(S)
+    S.append((i, j))
+    cnt += 1
 
 fig = go.Figure()
 for p1_idx, p2_idx in S:
     p1 = points[p1_idx]
     p2 = points[p2_idx]
-    fig.add_trace(go.Scatter(x=[p1[0], p2[0]], y=[p1[1], p2[1]], mode='lines'))
+    fig.add_trace(go.Scatter(x=[p1[0], p2[0]], y=[p1[1], p2[1]], 
+        marker=dict(
+            color='LightSkyBlue',
+            size=5,
+            line=dict(
+                color='MediumPurple',
+                width=2
+            )
+        ),
+        showlegend=False
+        )
+    )
+
+fig.update_yaxes(
+    scaleanchor = "x",
+    scaleratio = 1,
+  )
 
 fig.show()
+
+
+### Initialize an empty event Q. Next, insert the segment endpoints into Q
+
+
+
+
+
+
